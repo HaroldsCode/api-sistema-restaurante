8523ee975f197c06547bf17e8ea24db154a58d0d
Harold Moreno <haroldmoreno720@gmail.com>
2021-09-16 17:55:46 -0500

    implementación de helmet para proteger de vulnerabilidades más conocidas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 
@@ -8,6 +9,7 @@ const dataBase = require('./settings/mongo');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
